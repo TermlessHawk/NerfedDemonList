2b512a4e6b51893866f5e191e52d6633109c0099
TermlessHawk <gakne@wp.pl>
2026-05-24 17:16:56 +0200

    Update submission requirements to submission ruleset

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,30 +98,30 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3>Submission Ruleset</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks(including physics bypass, however FPS bypass is allowed).
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record on the level that is listed on the site - ULDM copies aren't allowed.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have clicks/taps recorded from your microphone in the video.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        A Cheat Indicator must be present in the video.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must show the previous attempt and the entire death animation, unless the completion is a first attempt.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Do not use bug routes.
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        If a list moderator is suspicious of your completion, they may ask for raw footage.
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        You can submit records on the NDL Discord server.
                     </p>
                 </div>
             </div>
