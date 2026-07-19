d40a68267a969d8db5c254658d32fa6064cd1d2a
Tosterr <marcelstukajajami@gmail.com>
2026-07-19 02:35:34 +0200

    Update recording requirements in List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -285,7 +285,7 @@ export default {
                     <p>
                         The recording must show the previous attempt, the entire end screen and death animation(unless the record is a first attempt).
                     </p>
-
+                        The video listed on the site must still show the full record from the raw footage.
                     <p>
                         Do not use bug routes.
                     </p>
